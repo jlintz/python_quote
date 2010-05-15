f0d6b6261c87793339e8258ee78675c9283e35ca
Justin Lintz <jlintz@gmail.com>
2010-05-14 23:12:04 -0400

    basic webapp for use of python quote in tornado, very very basic and still playing

---
+++ tornado_quote.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+Modified the Hello World tornado "webapp"
+"""
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import python_quote
+
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
+
+class MainHandler(tornado.web.RequestHandler):
+	"""
+		This will handle the default request or a GET request
+		looking for the "quotes" query string
+	"""
+	@tornado.web.asynchronous
+	def get(self):
+		qc = python_quote.QuoteCache()
+		quotes = self.get_argument("quotes").encode( "utf-8" )
+		quotes = list(quotes.split(","))
+		results = qc.get(quotes,['l1','a'],True)
+		self.write(results)
+		self.finish()
+
+class QuoteHandler(tornado.web.RequestHandler):
+	"""
+		Handles URIs of /quote/ticker
+		Single quotes only
+	"""
+	@tornado.web.asynchronous
+	def get(self,quote):
+		qc = python_quote.QuoteCache()
+		results = qc.get([quote],['l1','a'],True)
+		self.write(results)
+		self.finish()
+
+def main():
+	tornado.options.parse_command_line()
+	application = tornado.web.Application([
+        (r"/", MainHandler),
+		(r"/quote/([a-zA-Z]+)",QuoteHandler),
+    ])
+	http_server = tornado.httpserver.HTTPServer(application)
+	http_server.listen(options.port)
+	tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+	main()  
